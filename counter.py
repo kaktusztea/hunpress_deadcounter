133c52b4d2fbe8628b67c88726f82c9a19f871ca
kaktusztea <kaktusztea@protonmail.com>
2021-11-01 22:53:36 +0100

    Added blacklist function
    Added blacklist function

--- counter.py
+++ counter.py
@@ -4,31 +4,50 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_page_text(url):
     try:
         r = requests.get(url, verify=True, timeout=5)
     except requests.exceptions.ConnectionError:
         print("Request rejected. Skipping.")
-        return "False"
+        return False
     except requests.exceptions.ReadTimeout:
         print("No response from host. Skipping.")
-        return "False"
+        return False
     soup = BeautifulSoup(r.text, "html.parser")
     return soup.get_text()
 
-sites = ["https://telex.hu", "https://444.hu", "https://24.hu", "https://hvg.hu", "https://index.hu", \
-         "https://www.magyarhirlap.hu", "https://origo.hu", "https://magyarnemzet.hu", "https://hirado.hu"]
-words = ["meghal", "halott", "megöl", "áldozat"]
 
-for sitename in sites:
-    summa = 0
-    print("[%s]" % sitename)
-    text = get_page_text(sitename)
-    if text == "False":
-        print("")
-        continue
-    for word in words:
-        ct = text.count(word)
-        summa += ct
-        print("%s: %s" % (word, str(ct)))
-    print("(%s)\n" % str(summa))
+if __name__ == "__main__":
+
+    # Observered websites
+    sites = ["https://telex.hu", "https://444.hu", "https://24.hu", "https://hvg.hu", "https://index.hu", \
+            "https://www.magyarhirlap.hu", "https://origo.hu", "https://magyarnemzet.hu", "https://hirado.hu"]
+
+    # Searched keywords an their blacklisted exception phrases
+    words = {
+                 "meghal": ["meghall", "meghalad"],
+                 "halott": ["halottak napj", "halottaskocsi", "halottkém", "halottnak a csók", "halottkultusz"],
+                 "megöl": ["megölel"],
+                 "áldozat": ["áldozati póz"],
+                 "agyonvert": [],
+                 "meggyilkol": [],
+                 "gyilkos": ["gyilkos tréfa"]
+                }
+
+    # Iterate on sites, get their HTML content, create statistics
+    for sitename in sites:
+        summa = 0
+        bsumma = 0
+        print("\n[%s]" % sitename)
+        text = get_page_text(sitename)
+        if not text:
+            continue
+        for key in words.keys():
+            ct = bct = 0
+            ct = text.lower().count(key)
+            for bword in words[key]:
+                bct += text.lower().count(bword)
+            summa += (ct - bct)
+            print("%s: %s" % (key, str(ct - bct)))
+        print("(%s)" % str(summa))
