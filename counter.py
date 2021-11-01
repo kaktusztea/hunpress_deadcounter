ea0c5bdc603ddd6688fb3f68938cd677c4bfe9f0
kaktusztea <kaktusztea@protonmail.com>
2021-11-01 21:33:01 +0100

    Added exception handling
    Added exception handling

--- counter.py
+++ counter.py
@@ -5,7 +5,14 @@ from bs4 import BeautifulSoup
 import requests
 
 def get_page_text(url):
-    r = requests.get(url)
+    try:
+        r = requests.get(url, verify=True, timeout=5)
+    except requests.exceptions.ConnectionError:
+        print("Request rejected. Skipping.")
+        return "False"
+    except requests.exceptions.ReadTimeout:
+        print("No response from host. Skipping.")
+        return "False"
     soup = BeautifulSoup(r.text, "html.parser")
     return soup.get_text()
 
@@ -17,6 +24,9 @@ for sitename in sites:
     summa = 0
     print("[%s]" % sitename)
     text = get_page_text(sitename)
+    if text == "False":
+        print("")
+        continue
     for word in words:
         ct = text.count(word)
         summa += ct
