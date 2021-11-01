e26f1543e31b2726888e91cc173a833abccaecb7
kaktusztea <kaktusztea@protonmail.com>
2021-11-01 15:13:39 +0100

    Added more pages
    Added more pages

--- counter.py
+++ counter.py
@@ -9,7 +9,8 @@ def get_page_text(url):
     soup = BeautifulSoup(r.text, "html.parser")
     return soup.get_text()
 
-sites = ["https://telex.hu", "https://444.hu", "https://24.hu"]
+sites = ["https://telex.hu", "https://444.hu", "https://24.hu", "https://hvg.hu", "https://index.hu", \
+         "https://www.magyarhirlap.hu", "https://origo.hu", "https://magyarnemzet.hu", "https://hirado.hu"]
 words = ["meghal", "halott", "megöl", "áldozat"]
 
 for sitename in sites:
