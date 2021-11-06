051ed052154ee6e9dda25795f16b661860b39539
kaktusztea <kaktusztea@protonmail.com>
2021-11-06 23:42:28 +0100

    Added sqlite support
    Added sqlite support, storing stats in DB
    Polling in endless iteration
    Stop by CTRL+C

--- counter.py
+++ counter.py
@@ -2,7 +2,20 @@
 # -*- coding: utf-8 -*-
 
 from bs4 import BeautifulSoup
+import os
+import sys
+import time
+import signal
 import requests
+import sqlite3
+import datetime
+
+
+from enum import IntEnum
+class Detailed(IntEnum):
+    LOW = 1
+    MEDIUM = 2
+    HIGH = 3
 
 
 def get_page_text(url):
@@ -12,13 +25,105 @@ def get_page_text(url):
         print("Request rejected. Skipping.")
         return False
     except requests.exceptions.ReadTimeout:
-        print("No response from host. Skipping.")
+        print("No response from host [%s]. Skipping." % url)
         return False
     soup = BeautifulSoup(r.text, "html.parser")
     return soup.get_text()
 
 
-if __name__ == "__main__":
+def sql_init():
+    connection = None
+    dbfile = "stats.db"
+    if not os.path.isfile(dbfile):
+        connection = sql_connection(dbfile)
+        sql_table_init(connection)
+    else:
+        connection = sql_connection(dbfile)
+    return connection
+
+
+def sql_connection(dbfile):
+    try:
+        conn = sqlite3.connect(dbfile)
+        return conn
+    except Exception as Error:
+        print("[ERROR] Database connection failed. %s" % Error)
+        sys.exit(1)
+
+
+def sql_table_init(con):
+    cur = con.cursor()
+    cur.execute("CREATE TABLE stats(id integer PRIMARY KEY AUTOINCREMENT,  \
+                                    date TIMESTAMP NOT NULL, \
+                                    sitename TEXT NOT NULL,  \
+                                    word TEXT NOT NULL,       \
+                                    count INTEGER);")
+    con.commit()
+
+
+def sql_store(date, sitename, word, count):
+    if not connection:
+        print("[ERROR] No database connection. Exiting...")
+        sys.exit(1)
+    cr = connection.cursor()
+
+    data_tuple = (date, sitename, word, count)
+    sqlite_insert_with_param = "INSERT INTO 'stats' ('date', 'sitename', 'word', 'count') VALUES (?, ?, ?, ?);"
+    try:
+        cr.execute(sqlite_insert_with_param, data_tuple)
+        connection.commit()
+    except sqlite3.Error as err:
+        print("[SQLERROR] Failed INSERT operation. %s" % err)
+        connection.close()
+        sys.exit(1)
+
+
+def poll_sites(sites, words):
+    # Iterate on sites, get their HTML content, create statistics
+    for sitename in sites:
+        summa = 0
+        if detailed >= Detailed.MEDIUM:
+            print("\n[%s]" % sitename)
+        text = get_page_text(sitename)
+        if not text:
+            continue
+        dt = datetime.datetime.now()
+        for key in words.keys():
+            ct = bct = 0
+            ct = text.lower().count(key)
+            for bword in words[key]:
+                bct += text.lower().count(bword)
+            nct = (ct - bct)
+            summa += nct
+            if detailed == Detailed.HIGH:
+                print("%s: %s" % (key, str(nct)))
+            sql_store(dt, sitename, key, nct)
+        if detailed >= Detailed.MEDIUM:
+            print("(%s)" % str(summa))
+        sql_store(dt, sitename, "sum", summa)
+
+
+def ctrlc_handler(signum, frame):
+    print("\nPolling ended. Exiting.")
+    connection.close()
+    if debug:
+        print("[DEBUG] SQLite connection closed.")
+    sys.exit(1)
+
+
+def main():
+    global connection, detailed, debug         # SQLite connection, detailed mode, debug mode
+    period = 1                                 # polling period in minutes
+    iteration = 1                              # counter for polling loop
+    detailed = Detailed.HIGH
+    debug = True
+
+    connection = sql_init()
+    if connection and debug:
+        print("[DEBUG] SQLite connection opened.")
+    elif not connection:
+        print("[ERROR] No SQLite connection.")
+        sys.exit(1)
 
     # Observered websites
     sites = ["https://telex.hu", "https://444.hu", "https://24.hu", "https://hvg.hu", "https://index.hu",      \
@@ -36,19 +141,17 @@ if __name__ == "__main__":
                  "gyilkos": ["gyilkos tréfa"]
                 }
 
-    # Iterate on sites, get their HTML content, create statistics
-    for sitename in sites:
-        summa = 0
-        bsumma = 0
-        print("\n[%s]" % sitename)
-        text = get_page_text(sitename)
-        if not text:
-            continue
-        for key in words.keys():
-            ct = bct = 0
-            ct = text.lower().count(key)
-            for bword in words[key]:
-                bct += text.lower().count(bword)
-            summa += (ct - bct)
-            print("%s: %s" % (key, str(ct - bct)))
-        print("(%s)" % str(summa))
+    signal.signal(signal.SIGINT, ctrlc_handler)
+    
+    print("\nPolling starts, interval: %d minute(s)." % period)
+    print("Stop by pressing CTRL+C.\n")
+    while True:                                          # endless loop, break by CTRL+C
+        print("#%s." % str(iteration))
+        poll_sites(sites, words)
+        if debug:
+            print("\n[DEBUG] Sleeping %d seconds.\n" % 60 * period)
+        time.sleep(60 * period)
+        iteration += 1
+    
+if __name__ == "__main__":
+    main()
